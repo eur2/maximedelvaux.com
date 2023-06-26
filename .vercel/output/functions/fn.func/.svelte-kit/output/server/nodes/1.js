f3f00543a62c03e78265707f2331194ca5094d1e
eur2 <gregory.dapra@gmail.com>
2023-06-26 19:02:04 +0200

    rm .vercel folder

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,7 +0,0 @@
-
-
-export const index = 1;
-export const component = async () => (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.287ce18b.js","_app/immutable/chunks/scheduler.6a4f3e46.js","_app/immutable/chunks/index.6d7721eb.js","_app/immutable/chunks/stores.e9c81550.js","_app/immutable/chunks/singletons.ac8ba5f4.js"];
-export const stylesheets = [];
-export const fonts = [];
